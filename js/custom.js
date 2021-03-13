11b6946be7a76e28b1173546d68a906fb5e8b13d
firasabualkomboz <firasa018@gmail.com>
2021-03-13 12:31:18 +0200

    new tasks

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+
+var a = 50;
+var b = 20;
+var x = a * b ;
+
+
+document.getElementById('test').innerHTML = x;
+
+if(a === 0 ){
+    console.log('good feras');
+}else{
+    console.log('sorry this is false ');
+}
+
+document.getElementById("title").style.color = "green";
+document.getElementById("changetext").innerHTML = "My First JavaScript";
+function change() {
+    document.getElementById("changpar").innerHTML = "Paragraph changed.";
+}
+
+document.write(5 + 6);
+// window.alert("welcome feras !! do you want to subscribe ");
+// alert(5 + 6);
+console.log(5 + 6);
